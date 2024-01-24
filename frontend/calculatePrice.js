0e8b3cf60a0ea51e7135efa4af0aba6b2645ba56
Hugh Fraser <hughmm.fraser@gmail.com>
2024-01-24 22:54:59 +0000

    Adding validation because I'm picky.

--- frontend/calculatePrice.js
+++ frontend/calculatePrice.js
@@ -1,13 +1,21 @@
 const calculatePrice = () => {
     const salary = document.getElementById("salary").value || 32800;
     const days = document.getElementById("days").value;
-    console.log("Calculating price");
-    console.log(salary);
-    console.log(days);
-    let finalPrice = 0;
-    dailyRate = salary / 365;
-    finalPrice = priceRounder(dailyRate * days);
-    document.getElementById("finalPrice").innerHTML = finalPrice;
+
+    if (typeof salary === "number" && typeof days === "number") {
+        console.log("Calculating price");
+        console.log(salary);
+        console.log(days);
+        let finalPrice = 0;
+        dailyRate = salary / 365;
+        finalPrice = priceRounder(dailyRate * days);
+        document.getElementById("finalPrice").innerHTML = finalPrice;
+    } else {
+        console.log("Salary type: " + typeof salary);
+        console.log("Days type: " + typeof days);
+        document.getElementById("finalPrice").innerHTML = "Oops. Please try again and make sure your salary and number of days are both numbers.";
+    }
+    
 }
 
 const priceRounder = (finalPrice) => {
