8d7830365ded8af598ad90c757f3381e84bc0dc2
Hugh Fraser <hughmm.fraser@gmail.com>
2024-01-24 22:51:05 +0000

    Adding price rounder to nearest £100.

--- frontend/calculatePrice.js
+++ frontend/calculatePrice.js
@@ -1,11 +1,15 @@
 const calculatePrice = () => {
-    let salary = document.getElementById("salary").value || 32800;
-    let days = document.getElementById("days").value;
-    console.log("Calculating price")
-    console.log(salary)
-    console.log(days)
+    const salary = document.getElementById("salary").value || 32800;
+    const days = document.getElementById("days").value;
+    console.log("Calculating price");
+    console.log(salary);
+    console.log(days);
     let finalPrice = 0;
     dailyRate = salary / 365;
     finalPrice = dailyRate * days;
     document.getElementById("finalPrice").innerHTML = finalPrice;
+}
+
+const priceRounder = (finalPrice) => {
+    return Math.round(finalPrice / 100) * 100;
 }
