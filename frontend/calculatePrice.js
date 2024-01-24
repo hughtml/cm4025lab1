fd8bf23da351b8029e6d9ea4947411af53afedaa
Hugh Fraser <hughmm.fraser@gmail.com>
2024-01-24 22:32:02 +0000

    Default value for salary.

--- frontend/calculatePrice.js
+++ frontend/calculatePrice.js
@@ -1,5 +1,5 @@
 const calculatePrice = () => {
-    let salary = document.getElementById("salary").value;
+    let salary = document.getElementById("salary").value || 32800;
     let days = document.getElementById("days").value;
     console.log("Calculating price")
     console.log(salary)
