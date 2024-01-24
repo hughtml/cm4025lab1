26428517d13174dda05df60fc79cf6b1e9b655b6
Hugh Fraser <hughmm.fraser@gmail.com>
2024-01-24 22:57:35 +0000

    Getting rid of validation for now. Tiny refactor too.

--- frontend/calculatePrice.js
+++ frontend/calculatePrice.js
@@ -2,19 +2,12 @@ const calculatePrice = () => {
     const salary = document.getElementById("salary").value || 32800;
     const days = document.getElementById("days").value;
 
-    if (typeof salary === "number" && typeof days === "number") {
-        console.log("Calculating price");
-        console.log(salary);
-        console.log(days);
-        let finalPrice = 0;
-        dailyRate = salary / 365;
-        finalPrice = priceRounder(dailyRate * days);
-        document.getElementById("finalPrice").innerHTML = finalPrice;
-    } else {
-        console.log("Salary type: " + typeof salary);
-        console.log("Days type: " + typeof days);
-        document.getElementById("finalPrice").innerHTML = "Oops. Please try again and make sure your salary and number of days are both numbers.";
-    }
+    console.log("Calculating price");
+    console.log(salary);
+    console.log(days);
+    const dailyRate = salary / 365;
+    const finalPrice = priceRounder(dailyRate * days);
+    document.getElementById("finalPrice").innerHTML = finalPrice;
     
 }
 
